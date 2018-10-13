bd63e6d7c54c6e9ba4278b94c1e859c45bb9bb1e
Jovan Ivanov <jokinta011@gmail.com>
2018-10-13 19:52:39 +0300

    Inheritance

---
+++ Inheritance/animal/Animal.java
@@ -0,0 +1,7 @@
+package itheritance.animal;
+
+public class Animal {
+    public void eat(){
+        System.out.println("eating...");
+    }
+}
---
+++ Inheritance/animal/Dog.java
@@ -0,0 +1,7 @@
+package itheritance.animal;
+
+public class Dog extends Animal{
+    public void bark(){
+        System.out.println("barking...");
+    }
+}
